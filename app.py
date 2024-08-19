e55d8d0fd1b939a34b0723611cacc9737c4571af
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2024-08-19 13:33:14 +0800

    added docker+boards of multiple sizes

--- app.py
+++ app.py
@@ -7,41 +7,74 @@ socketio = SocketIO(app)
 
 rooms = {}
 
+def check_winner(board, size):
+    # Check rows, columns and diagonals for a winner
+    for i in range(size):
+        if all(board[i*size + j] == board[i*size] and board[i*size] != '' for j in range(size)):
+            return True
+        if all(board[j*size + i] == board[i] and board[i] != '' for j in range(size)):
+            return True
+    if all(board[i*size + i] == board[0] and board[0] != '' for i in range(size)):
+        return True
+    if all(board[i*size + (size-i-1)] == board[size-1] and board[size-1] != '' for i in range(size)):
+        return True
+    return False
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/room/<room_id>')
-def room(room_id):
+@app.route('/room/<room_id>/<int:size>')
+def room(room_id, size):
     if room_id not in rooms:
         rooms[room_id] = {
             'players': [],
+            'board': [''] * (size * size),
+            'size': size,
             'turn': 1  # Player 1 starts
         }
-    return render_template('room.html', room_id=room_id)
+    return render_template('room.html', room_id=room_id, size=size)
 
 @socketio.on('join')
 def handle_join(data):
     room_id = data['room_id']
+    size = data['size']
     join_room(room_id)
 
     if len(rooms[room_id]['players']) < 2:
         player_number = len(rooms[room_id]['players']) + 1
         rooms[room_id]['players'].append({'sid': request.sid, 'player_number': player_number})
         emit('set_player', player_number)
-        emit('room_joined', {'room_id': room_id, 'player_number': player_number}, room=room_id)
     else:
-        emit('room_full')
+        emit('spectator', {'size': rooms[room_id]['size']})
+
+    emit('room_joined', {'room_id': room_id, 'board': rooms[room_id]['board'], 'size': size}, room=room_id)
 
 @socketio.on('make_move')
 def handle_make_move(data):
     room_id = data['room_id']
     move = data['move']
+    size = rooms[room_id]['size']
     player = next((player for player in rooms[room_id]['players'] if player['sid'] == request.sid), None)
 
     if player and rooms[room_id]['turn'] == player['player_number']:
-        emit('move_made', {'move': move, 'player': player['player_number']}, room=room_id)
-        rooms[room_id]['turn'] = 2 if player['player_number'] == 1 else 1  # Toggle turn
+        if rooms[room_id]['board'][move] == '':
+            rooms[room_id]['board'][move] = 'X' if player['player_number'] == 1 else 'O'
+            if check_winner(rooms[room_id]['board'], size):
+                emit('game_over', {'winner': player['player_number']}, room=room_id)
+            elif all(cell != '' for cell in rooms[room_id]['board']):
+                emit('game_over', {'winner': 0}, room=room_id)
+            else:
+                rooms[room_id]['turn'] = 2 if player['player_number'] == 1 else 1
+                emit('move_made', {'move': move, 'player': player['player_number']}, room=room_id)
+
+@socketio.on('rematch')
+def handle_rematch(data):
+    room_id = data['room_id']
+    size = rooms[room_id]['size']
+    rooms[room_id]['board'] = [''] * (size * size)
+    rooms[room_id]['turn'] = 1  # Player 1 starts
+    emit('start_rematch', {'size': size}, room=room_id)
 
 @socketio.on('disconnect')
 def handle_disconnect():
